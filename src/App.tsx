038085773c20664c8019eee84271c46cc50fc9fb
Aintzane <aint.iglesias@gmail.com>
2025-12-31 18:39:07 +0100

    Add header and main with classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import IssuesPage from "./features/issues/pages/IssuesPage";
-
+import Header from "./components/header";
 function App() {
   return (
-    <>
+    <main className="bg-gh-bg text-gh-text min-h-screen">
+      <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/issues" replace />} />
         <Route path="/issues" element={<IssuesPage />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
