6d5c300b9f88ef8060c9b3deada21099de0590da
Aintzane <aint.iglesias@gmail.com>
2026-01-01 16:45:53 +0100

    Refactor issues item list to component

--- src/features/issues/pages/IssuesPage.tsx
+++ src/features/issues/pages/IssuesPage.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from "@apollo/client/react";
 import { REPO_ISSUES_QUERY } from "../../../api/queries/issues";
-import type { IssuesQueryData, Issue } from "../../../types/types";
-import { formatTime } from "../../../helpers/helpersFunctions";
-import { Link } from "react-router-dom";
+import type { IssuesQueryData } from "../../../types/types";
+import IssuesListItem from "../components/IssuesListItem";
 
 export default function IssuesPage() {
   const {
@@ -55,48 +54,10 @@ export default function IssuesPage() {
                 </button>
               </>
             ) : (
-              <div className="issues-list-cotainer">
+              <div className="issues-list-container">
                 <ul>
                   {issues.map((issue, index) => (
-                    <li
-                      className={`border-gh-muted px-4 py-2 ${index === issues.length - 1 ? "border-none" : "border-b"}`}
-                      key={issue.id}
-                    >
-                      <div className="issue-item flex gap-2">
-                        <div className="issue-icon"></div>
-                        <div>
-                          <div className="flex flex-wrap items-center gap-1">
-                            <Link to={`${issue.number}`} className="text-base font-medium">
-                              {issue.title}
-                            </Link>
-                            {issue.labels.nodes.length > 0 && (
-                              <div className="flex items-center gap-1">
-                                {issue.labels.nodes.map((label) => (
-                                  <span key={label.id} className="issue-label text-xs font-medium">
-                                    {label.name}
-                                  </span>
-                                ))}
-                              </div>
-                            )}
-                          </div>
-                          <div className="mt-1 flex gap-1">
-                            <p className="text-gh-gray text-xs">#{issue.number}</p>
-                            <p className="text-gh-gray text-xs">·</p>
-                            <p className="text-gh-gray text-xs">
-                              {issue.author?.login ?? "unknown"}
-                            </p>
-                            <p className="text-gh-gray text-xs">
-                              opened{formatTime(issue.createdAt)}{" "}
-                            </p>
-                            <p className="text-gh-gray text-xs">·</p>
-
-                            <p className="text-gh-gray text-xs">
-                              {issue.updatedAt ? "Updated " + formatTime(issue.updatedAt) : ""}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
+                    <IssuesListItem issue={issue} isLast={index === issues.length - 1} />
                   ))}
                 </ul>
               </div>
