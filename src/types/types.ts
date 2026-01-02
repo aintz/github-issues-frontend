08bb1e7a579e84b750cabf2bbd84e7d92aeb47c6
Aintzane <aint.iglesias@gmail.com>
2026-01-02 14:00:51 +0100

    Delete the Issue type, and change it into the Query type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,20 +1,10 @@
-export interface Issue {
-  id: string;
-  number: number;
-  title: string;
-  state: "OPEN" | "CLOSED";
-  updatedAt: string;
-  createdAt: string;
-  comments: { totalCount: number };
-  author?: { login: string } | null;
-  labels: { nodes: Array<{ id: string; name: string; color: string }> };
-}
+import type { IssueFieldsFragment } from "../generated/graphql";
 
 export interface IssuesQueryData {
   repository: {
     issues: {
       totalCount: number;
-      nodes: Issue[] | null;
+      nodes: IssueFieldsFragment[] | null;
     } | null;
     openIssues: { totalCount: number } | null;
     closedIssues: { totalCount: number } | null;
