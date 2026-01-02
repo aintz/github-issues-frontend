9fc20d4b0b988e4775fea10bd62fa2ca5d95bcf2
Aintzane <aint.iglesias@gmail.com>
2026-01-02 13:59:44 +0100

    Change the fields for the fragment and added the id to query to fix the merge issue

--- src/api/queries/issues.ts
+++ src/api/queries/issues.ts
@@ -1,6 +1,8 @@
 import { gql } from "@apollo/client";
+import { ISSUE_FIELDS_FRAGMENT } from "../fragments/issue";
 
 export const REPO_ISSUES_QUERY = gql`
+  ${ISSUE_FIELDS_FRAGMENT}
   query Issues(
     $owner: String!
     $name: String!
@@ -10,6 +12,7 @@ export const REPO_ISSUES_QUERY = gql`
     $orderBy: IssueOrder = { field: UPDATED_AT, direction: DESC }
   ) {
     repository(owner: $owner, name: $name) {
+      id
       openIssues: issues(states: [OPEN]) {
         totalCount
       }
@@ -19,18 +22,7 @@ export const REPO_ISSUES_QUERY = gql`
       issues(first: $first, after: $after, states: $states, orderBy: $orderBy) {
         totalCount
         nodes {
-          id
-          number
-          title
-          state
-          updatedAt
-          createdAt
-          comments {
-            totalCount
-          }
-          author {
-            login
-          }
+          ...IssueFields
           labels(first: 5) {
             nodes {
               id
